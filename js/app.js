a9d2a3147860de21142e5a91a7038083f2d91523
Daler <murtozoevdaler@gmail.com>
2023-01-23 20:31:52 +0500

    prikol

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-let lefteye = document.querySelectorAll('#lefteye')
-let righteye = document.querySelectorAll('#righteye')
-const head = document.querySelector('#head')
+let eye = document.querySelectorAll('.eye')
+// let righteye = document.querySelectorAll('.eye')
+const head = document.querySelector('.head')
 
 head.addEventListener('mousemove',(event)=>{
-    head.forEach(item =>{
+    eye.forEach(item =>{
       item.style.left = event.pageX + "px"
       item.style.right = event.pageX + "px"
       item.style.top = event.pageY + "px"
